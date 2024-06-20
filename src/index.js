3a5701034eb12c1df6685495772bd4b309a69b48
朱治龙 <zzlongqqboy@163.com>
2024-06-20 14:40:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
+  "docker.520963.xyz": dockerHub,
+  "quay.520963.xyz": "https://quay.io",
+  "gcr.520963.xyz": "https://gcr.io",
+  "k8s-gcr.520963.xyz": "https://k8s.gcr.io",
+  "k8s.520963.xyz": "https://registry.k8s.io",
+  "ghcr.520963.xyz": "https://ghcr.io",
   "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.520963.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
